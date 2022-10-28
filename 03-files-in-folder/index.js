525f4a8288b8c2bb0bd211a35a648b692fb0c24b
Elijah <elijah.ivanik@inbox.ru>
2022-10-28 09:16:23 +0300

    feat: complete task-03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+// dependencies
+const path = require("path")
+const fs = require("fs")
+
+// var
+const { stdout } = process
+const dir = path.join(__dirname, "secret-folder")
+
+// readdir
+fs.readdir(dir, (_, files) => {
+  files.forEach((file) => {
+    // get name + ext
+    const info = path.parse(file)
+
+    fs.stat(path.resolve(dir, file), (_, stats) => {
+      // handle ONLY files
+      if (!stats.isFile()) {
+        return
+      }
+
+      // get file size
+      const size = stats.size / 1000
+
+      // out formatted file info
+      stdout.write(`${info.name} - ${info.ext.replace(".", "")} - ${size}kb\n`)
+    })
+  })
+})
